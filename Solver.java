3258362e9de8a72bc302bcbdd0a13c9a725c72db
Arlich Conde <abconde1998@gmail.com>
2020-02-12 00:16:07 -0600

    creacion de repositorio

---
+++ Solver.java
@@ -0,0 +1,22 @@
+/*
+    Esta es su clase principal. El unico metodo que debe implementar es
+    public String[] solve(Maze maze)
+    pero es libre de crear otros metodos y clases en este u otro archivo que desee.
+*/
+public class Solver{
+    private int maxMoves;
+    private int exitSpace;
+
+
+    public Solver(){
+        //Sientase libre de implementar el contructor de la forma que usted lo desee
+    }
+
+    public String[] solve(Maze maze){
+        //Implemente su metodo aqui. Sientase libre de implementar métodos adicionales
+        String[] str = new String[1];
+        str[0] = "[-1]";
+        return str;
+    }
+
+}
